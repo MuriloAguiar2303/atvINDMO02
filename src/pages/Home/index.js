433c69103baa986f800615715878b6542d69c4d1
unknown <murilo.aguiar2303@gmail.com>
2024-02-07 21:27:00 -0300

    Enviando tudo

---
+++ src/pages/Home/index.js
@@ -0,0 +1,28 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import {Text,Button,Image,StyleSheet} from 'react-native'
+
+export default function Home() {
+    return (
+        <SafeAreaView style={styles.container}>
+            <Image source={require("../../assets/logo-senai.png")} style={{width:250, height:130,position:'absolute',top:2}}></Image>
+            <Text>Murilo Henrique de Aguiar</Text>
+            <Text>Sumaré, 07/02/2024</Text>
+            <Text>Escola SENAI Celso Charuri</Text>
+            <Text>Técnico em Desenvolvimento de Sistemas</Text>
+            <Text>INDMO - Interface para Dispositivos Móveis</Text>
+        </SafeAreaView>
+    )
+  }
+
+
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor:'#808080'
+    },
+  });
+  
